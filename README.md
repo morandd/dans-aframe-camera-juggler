08c714abde38a3d857fd4c01c51ad331d487a60c
DansWindowsPC <moran@kgm-associates.com>
2017-05-16 09:55:15 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # dans-aframe-camera-juggler
-AFrame controller to activate the appropriate camera in desktop, mobile, and VR modes
+AFrame component to activate the appropriate camera controller in desktop, mobile, and VR modes
+
+AFrame offers a number of camera controllers for mouse, touch, and free-look, but there is no one controller that works intuitively in desktop, mobile, and VR mode. This component offers a solution. 
+
+The necessity that birthed this invention is as follows: I want to show off a 3D object on a web site. It should work on the desktop, on a phone/tablet, and with a VR headset/Google Cardboard. Some scenes I'd like to show with orbit cam, and other scenes should be seen
+with a free-look camera. This component exists to juggle cameras as needed to accomplish this goal.
+
+There are two paradigms for this controller: `orbit` and `freelook`.
+
+I. Orbit-controls paradigm
+	A. On Desktop:
+		1. start: use orbit-cam
+		2. enter-vr: Switch cam to look-cam. 
+		3. exit-vr: Switch to orbit-cam
+	B. On mobile:
+		1. start: use orbit-cam
+		2. enter-vr: Switch cam to look-cam. 
+		3. exit-vr: Switch to orbit-cam
+
+II. Look-controls paradigm
+	A. On Desktop:
+		1. start: Use look-cam
+		2. enter-vr: Remain in look-cam
+		3. exit-vr: Remain in look-cam. Reset camera pitch to 0'. 
+	B. On mobile:
+		1. start: Use multitouch-look-cam*
+		2. enter-vr: Switch to look-cam
+		3. exit-vr: Switch to multitouch-look-cam
+
+* There was no good free-look controller designed for touch input, so I built one: (multitouch-look-controls). The AFrame look-controls on mobile uses the 'magic window' paradigm, taking camera controls from the phone deviceorientation. But in some cases it's more usable to have a touch-driven look controller, so folks can explore with their fingers instead of waving the phone about. 
+(multitouch-look-controls) provides yaw, pitch, dolly, and pinch to zoom.
+
+
+# API #
+Attribute | Description | Default
+--- | --- | ---
+paradigm | Required. Either 'freelook' or 'orbit' | ''
+orbitCamID | DOM id of the camera with orbit controls | #camera_orbit
+lookCamID | DOM id of the camera with look-controls | #camera_look
+touchlookCamID | DOM id of the camera with multtouch-look-controls | #camera_touchlook
+
+
+# Using #
+
+Include in page:
+```
+<script src="https://morandd.github.io/dans-aframe-camera-juggler/dans-camera-juggler.js"></script>
+```
+Then add it to your `a-scene`. To use it, set up three cameras (or two, if you don't ned the multitouch-look cam).
+
+````
+	<script src="https://cdn.rawgit.com/tizzle/aframe-orbit-controls-component/v0.1.6/dist/aframe-orbit-controls-component.js"></script>
+
+<a-scene dans-aframe-camera-juggler="paradigm:orbit">
+<a-entity camera="active:true" orbit-controls
+
+```
---
+++ dans-camera-juggler.js
@@ -0,0 +1,182 @@
+
+/*
+
+
+I want to show off a 3D object on a web site. It should work on the desktop, on a phone/tablet,
+and with a VR headset/Google Cardboard. Some scenes I want to show using orbit controls,
+and some scenes I want to use a free look camera. 
+
+This component exists to juggle camera controls as needed to accomplish that. We need to switch
+between several cameras in AFrame (or to add/remove controllers to a single camera)
+
+
+I. Orbit-controls paradigm
+	A. On Desktop:
+		1. start: use orbit controls
+		2. enter-vr: Switch active cam to look-controls. 
+		3. exit-vr: Switch active to orbit cam
+	B. On mobile:
+		start: use orbit-controls
+		enter-vr: Switch active cam to look-controls. 
+		exit-vr: Switch active to orbit cam
+
+II. Look-controls paradigm
+	A. On Desktop:
+		1. start: use look-controls
+		2. enter-vr: Remain in look-controls mode
+		3. exit-vr: Reset camera pitch to 0'. Remain in look-controls mode
+	B. On mobile:
+		1. start: use multitouch-look-controls*
+		2. enter-vr: switch to regular look-controls
+		3. exit-vr: switch to multitouch-look-controls
+
+*multitouch-look-controls adds bounded +/- pitch as a touch gesture, and adds pinch-to-dolly as a gesture
+
+*/
+
+
+AFRAME.registerComponent('dans-camera-juggler', {
+
+	schema: {
+		paradigm:   { type: 'string', default:''}, // Can be 'freelook' or 'orbit'
+		orbitCamID: { type: 'string', default:"#camera_orbit" },
+		lookCamID:  { type: 'string', default:"#camera_look" },
+		touchlookCamID:  { type: 'string', default:"#camera_touchlook" },
+	},
+
+
+	// This component should only be instantiated once per scene
+	multiple: false,
+
+
+	switchToOrbitCam: function () {
+
+		 console.log('switchToOrbitCam');
+
+		this.data.orbitCamEl.setAttribute('camera',{active: true});
+		this.data.orbitCamEl.play(); // We must call play() and pause() in addition to setting the 'active' property. This may change in the future: https://github.com/aframevr/aframe/issues/2671
+		this.data.lookCamEl.setAttribute('camera',{active: false});
+		this.data.lookCamEl.pause();
+		this.data.tlookCamEl.setAttribute('camera',{active: false});
+		this.data.tlookCamEl.pause();
+
+	},
+
+
+	switchToLookCam: function () {
+
+		 console.log('switchToLookCam');
+
+		this.data.orbitCamEl.setAttribute('camera',{active: false});
+		this.data.orbitCamEl.pause();
+		this.data.lookCamEl.setAttribute('camera',{active: true});
+		this.data.lookCamEl.play();
+		this.data.tlookCamEl.setAttribute('camera',{active: false});
+		this.data.tlookCamEl.pause();
+
+	},
+
+	switchToTouchlookCam: function () {
+
+		 console.log('switchToTouchlookCam');
+
+		this.data.orbitCamEl.setAttribute('camera',{active: false});
+		this.data.orbitCamEl.pause();
+		this.data.lookCamEl.setAttribute('camera',{active: false});
+		this.data.lookCamEl.pause();
+		this.data.tlookCamEl.setAttribute('camera',{active: true});
+		this.data.tlookCamEl.play();
+
+	},
+
+
+	init: function(){
+
+		this.data.orbitCamEl = document.querySelector(this.data.orbitCamID);
+		this.data.lookCamEl = document.querySelector(this.data.lookCamID);
+		this.data.tlookCamEl = document.querySelector(this.data.touchlookCamID);
+
+		/*
+		I. Orbit-controls paradigm
+		*/
+		if (this.data.paradigm==='orbit') {
+
+			// I.B On mobile:
+			if (AFRAME.utils.device.isMobile()) {
+
+				// I.B.1. start: use orbit-controls
+				this.switchToOrbitCam();
+				// We have to call this again after 1ms because the initialization sequence (scene, cameras, etc) is not clear...
+				setTimeout(this.switchToOrbitCam.bind(this), 1);
+
+				//  I.B.2. enter-vr: save orbit cam position. Switch active cam to look-controls. 
+				AFRAME.scenes[0].addEventListener('enter-vr', this.switchToLookCam.bind(this));
+
+				//  I.B.3. exit-vr: save look-control position. Restore orbit-cam position. Switch active to orbit cam
+				AFRAME.scenes[0].addEventListener('exit-vr', this.switchToOrbitCam.bind(this));
+
+			//I.A On Desktop:
+			} else {
+
+				//I.A.1. start: use orbit controls
+				this.switchToOrbitCam();
+				// We have to call this again after 1ms because the initialization sequence (scene, cameras, etc) is not clear...
+				setTimeout(this.switchToOrbitCam.bind(this), 1);
+
+				//I.A.2. enter-vr: save orbit cam position. Switch active cam to look-controls. 
+				AFRAME.scenes[0].addEventListener('enter-vr', this.switchToLookCam.bind(this));
+
+				//I.A.3. exit-vr: save look-control position. Restore orbit-cam position. Switch active to orbit cam
+				AFRAME.scenes[0].addEventListener('exit-vr', this.switchToOrbitCam.bind(this));
+
+			} // mobile or desktop?
+
+		} // end if paradigm=='orbit'
+
+
+		/*
+		II. Look-controls paradigm
+		*/
+		else if (this.data.paradigm==='freelook') {
+
+			// II.B On mobile:
+			if (AFRAME.utils.device.isMobile()) {
+				// II.B.1. start: use multitouch-look-controls*
+				this.switchToTouchlookCam();
+				setTimeout(this.switchToTouchlookCam.bind(this), 1);
+
+				// II.B.2. enter-vr: switch to regular look-controls
+				AFRAME.scenes[0].addEventListener('enter-vr', this.switchToLookCam.bind(this));
+
+				//	3. exit-vr: switch to multitouch-look-controls
+				AFRAME.scenes[0].addEventListener('exit-vr', this.switchToTouchlookCam.bind(this));
+
+			// II.A On Desktop:
+			} else {
+				// II.A.1. start: use look-controls
+				this.switchToLookCam();
+				setTimeout(this.switchToLookCam.bind(this), 1);
+
+				// II.A.2. enter-vr: Remain in look-controls mode
+					// do nothing.
+
+				// II.A.3. exit-vr: Reset camera pitch to 0'. Remain in look-controls mode
+					// do nothing.
+
+			} // mobile or desktop?
+
+		} // end if paradigm=='freelook'
+
+
+		/*
+			III. Other paradigms can be implemented in the future.
+		*/
+		else {
+			console.error('Unknown paradigm (must be freelook or orbit): '  + this.data.paradigm);
+		}
+
+
+
+	} // end init() function
+});
+
